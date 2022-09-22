671c09027407fac6fd477b5a3e0feb39093ed9c7
iZeroKim <kimgichuru6@gmail.com>
2022-09-22 12:50:53 +0300

    Get computer choice

--- game.go
+++ game.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func displayHeading(){
 	fmt.Println("Rock, Paper, Scissors!")
 }
+
+func getComputerChoice() string{
+	var compChoice string
+	min := 1
+	max := 4
+
+	rand.Seed(time.Now().UnixNano())
+	randomInt := rand.Intn(max - min) + min
+
+	if randomInt == 1 {
+		compChoice = "rock"
+	} else if randomInt == 2 {
+		compChoice = "paper"
+	} else {
+		compChoice = "scissors"
+	}
+	return compChoice
+
+}
+
 func main(){
 	displayHeading()
 	fmt.Println("Hello, welcome to the game\n")
@@ -14,12 +38,13 @@ func main(){
 	var isRunning bool
 	isRunning = true
 
+	fmt.Println(getComputerChoice())
+
 	for isRunning {
-		fmt.Println("Enter Rock, Paper or Scissor. Enter End to end the game")
+		fmt.Printf("Enter Rock, Paper or Scissor. Enter End to end the game: ")
 
 		var choice string
 		fmt.Scan(&choice)
-
 		if choice == "end" {
 			fmt.Println("Game Ended")
 			isRunning = false
