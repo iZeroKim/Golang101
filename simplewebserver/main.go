dbf0be088eba6f0dc50d0b2f62286cf88ce48b64
iZeroKim <kimgichuru6@gmail.com>
2022-09-27 05:39:00 +0300

    Web server : Handle index route

--- simplewebserver/main.go
+++ simplewebserver/main.go
@@ -5,7 +5,12 @@ import (
 	"net/http"
 )
 
+func index(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
 func main(){
 	fmt.Println("Simple Go Web Server")
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":3000", nil)
 }
