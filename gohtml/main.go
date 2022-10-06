97a04f8134a084aca27372893fcf1591b6691607
iZeroKim <kimgichuru6@gmail.com>
2022-10-06 10:30:24 +0300

    Gohtml: Add html document variable

--- gohtml/main.go
+++ gohtml/main.go
@@ -11,6 +11,28 @@ type Info struct{
 }
 
 func main(){
+
+	const doc =`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		<h3>Hi, {{.Name}}. The fruits are:</h3>
+		<ul>
+			{{range .Fruit}}
+				<li>
+					{{.}}
+				</li>
+			{{end}}
+		</ul>
+	</body>
+	</html>
+	`
 	fmt.Println("Starting new server at: http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
