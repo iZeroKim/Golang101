94f9af4fe76736d62b5865b17c35ece1306713e4
iZeroKim <kimgichuru6@gmail.com>
2022-10-06 10:26:08 +0300

    Gohtml: Create Info struct

--- gohtml/main.go
+++ gohtml/main.go
@@ -4,6 +4,12 @@ import ("net/http"
 		"fmt"
 )
 
+type Info struct{
+	Title string
+	Name string
+	Fruits [3]string
+}
+
 func main(){
 	fmt.Println("Starting new server at: http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
