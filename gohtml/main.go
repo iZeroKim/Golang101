4b83b2966b0f1bdd2f79d7b706ada30608838705
iZeroKim <kimgichuru6@gmail.com>
2022-10-07 21:36:07 +0300

    Gohtml: Comment out html template code

--- gohtml/main.go
+++ gohtml/main.go
@@ -6,50 +6,50 @@ import ("net/http"
 		"github.com/gorilla/mux"
 )
 
-type Info struct{
-	Title string
-	Name string
-	Fruits [3]string
-}
+// type Info struct{
+// 	Title string
+// 	Name string
+// 	Fruits [3]string
+// }
 
 func main(){
 
-	const doc =`
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>{{.Title}}</title>
-	</head>
-	<body>
-		<h3>Hi, {{.Name}}. The fruits are:</h3>
-		<ul>
-			{{range .Fruits}}
-				<li>
-					{{.}}
-				</li>
-			{{end}}
-		</ul>
-	</body>
-	</html>
-	`
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
-		w.Header().Add("Content Type", "text/html")
-		templates:= template.New("template")
-		templates.New("doc").Parse(doc)
-		info := Info{
-			"My Fruits",
-			"Kim",
-			[3]string{
-				"Sweet melon",
-				"Water melon",
-				"Pineapple",
-			},
-		}
-		templates.Lookup("doc").Execute(w, info)
-	})
-	fmt.Println("Starting new server at: http://localhost:8000")
-	http.ListenAndServe(":8000", nil)
+	// const doc =`
+	// <!DOCTYPE html>
+	// <html lang="en">
+	// <head>
+	// 	<meta charset="UTF-8">
+	// 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	// 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	// 	<title>{{.Title}}</title>
+	// </head>
+	// <body>
+	// 	<h3>Hi, {{.Name}}. The fruits are:</h3>
+	// 	<ul>
+	// 		{{range .Fruits}}
+	// 			<li>
+	// 				{{.}}
+	// 			</li>
+	// 		{{end}}
+	// 	</ul>
+	// </body>
+	// </html>
+	// `
+	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+	// 	w.Header().Add("Content Type", "text/html")
+	// 	templates:= template.New("template")
+	// 	templates.New("doc").Parse(doc)
+	// 	info := Info{
+	// 		"My Fruits",
+	// 		"Kim",
+	// 		[3]string{
+	// 			"Sweet melon",
+	// 			"Water melon",
+	// 			"Pineapple",
+	// 		},
+	// 	}
+	// 	templates.Lookup("doc").Execute(w, info)
+	// })
+	// fmt.Println("Starting new server at: http://localhost:8000")
+	// http.ListenAndServe(":8000", nil)
 }
